355b5717ada5f051c76a4b71850f852d371879f7
OleksiiKyrianov <oleksii.kyrianov@infopulse.com>
2024-10-24 21:07:53 +0300

    markup main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideBarComponent} from "./layouts/side-bar/side-bar.component";
+import {HeaderComponent} from "./layouts/header/header.component";
+import {FooterComponent} from "./layouts/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'petUdemi';
+
+  ngOnInit(){
+    const userLanguages: readonly string[] = navigator.languages;
+    console.log(userLanguages)
+  }
 }
