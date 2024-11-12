dc582b86c0ef1b352f92be0a0db9c1451864e79c
o.kyrianov <o.kyrianov@fora.ua>
2024-11-12 09:26:01 +0200

    [feat](main page): add markup to site

--- src/app/pages/mainPage/main-page.component.ts
+++ src/app/pages/mainPage/main-page.component.ts
@@ -3,13 +3,21 @@ import {CommonModule} from '@angular/common';
 import {MatIcon} from "@angular/material/icon";
 import {MatButtonModule, MatFabButton} from "@angular/material/button";
 import {MatDivider} from "@angular/material/divider";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: 'main-page.component.html',
   styleUrl: 'main-page.component.scss',
   standalone: true,
-  imports: [CommonModule, MatIcon, MatButtonModule, MatDivider],
+  imports: [CommonModule, MatIcon, MatButtonModule, MatDivider, MatCardActions, MatCardContent, MatCardSubtitle, MatCardTitle, MatCardHeader, MatCard],
 })
 export class MainPageComponent {
 }
