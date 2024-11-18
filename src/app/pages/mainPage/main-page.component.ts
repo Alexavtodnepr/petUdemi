da342baa12423bc593b7c5e8272fd0b9c5ee6dd5
o.kyrianov <o.kyrianov@fora.ua>
2024-11-19 00:01:32 +0200

    [feat](main page): change adaptive video background

--- src/app/pages/mainPage/main-page.component.ts
+++ src/app/pages/mainPage/main-page.component.ts
@@ -39,25 +39,22 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
     private video!: HTMLVideoElement;
     private videoTexture!: THREE.VideoTexture;
     private animationId!: number;
-    windowWidth: number = window.innerWidth;
+    windowWidth: number = window.innerWidth<= 950? 950 : window.innerWidth;
+    canvasSize: number = window.innerWidth <= 500 ? 500 : window.innerWidth;
     windowHeight: number = window.innerHeight;
 
     @HostListener('window:resize', ['$event'])
     onResize(event: Event): void {
-        this.windowWidth = (event.target as Window).innerWidth;
+        this.windowWidth = (event.target as Window).innerWidth <= 950? 950 : (event.target as Window).innerWidth;
         this.windowHeight = (event.target as Window).innerHeight;
-
-        // Обновляем соотношение сторон камеры
         this.camera.aspect = this.windowWidth / this.windowHeight;
         this.camera.updateProjectionMatrix();
 
-        // Обновляем размер рендерера
         this.renderer.setSize(this.windowWidth, this.windowHeight);
 
-        // Корректировка масштаба плоскости, чтобы она оставалась пропорциональной
         const scaleX = this.windowWidth / 200;
         const scaleY = this.windowHeight / 200;
-        const plane = this.scene.children[0]; // Получаем первую добавленную плоскость (в данном случае это видео плоскость)
+        const plane = this.scene.children[0];
         plane.scale.set(scaleX, scaleY, 1);
     }
 
@@ -75,7 +72,7 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
         this.renderer.dispose();
         if (this.video) {
             this.video.pause();
-            this.video.removeAttribute('src'); // Очистка источника видео
+            this.video.removeAttribute('src');
             this.video.load();
         }
     }
@@ -93,16 +90,14 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
         this.video.style.position = 'absolute';
         this.video.style.top = '50%';
         this.video.style.left = '50%';
-        this.video.style.transform = 'translate(-50%, -50%)'; // Центрируем видео
-        this.video.style.width = '1980px'; // Ширина видео всегда будет 100%
-        this.video.style.height = 'auto'; // Высота будет автоматически вычисляться с сохранением пропорций
-        this.video.style.minWidth = '100vw'; // Минимальная ширина видео — ширина экрана
-        this.video.style.minHeight = '100vh'; // Минимальная высота видео — высота экрана
-        this.video.style.objectFit = 'cover'; // Видео будет масштабироваться, сохраняя пропорции
-        this.video.style.overflow = 'hidden'; // Видео будет масштабироваться, сохраняя пропорции
+        this.video.style.transform = 'translate(-50%, -50%)';
+        this.video.style.width = '100vw';
+        this.video.style.height = 'auto';
+        this.video.style.minWidth = '100vw';
+        this.video.style.minHeight = '100vh';
+        this.video.style.objectFit = 'cover';
+        this.video.style.overflow = 'hidden';
 
-
-        // Создаем текстуру для THREE.js
         this.videoTexture = new THREE.VideoTexture(this.video);
         this.videoTexture.minFilter = THREE.LinearFilter;
         this.videoTexture.magFilter = THREE.LinearFilter;
@@ -110,7 +105,6 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     private initThree(): void {
-        // Создаем сцену, камеру и рендерер
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, this.windowWidth / this.windowHeight, 0.1, 1000);
         this.camera.position.z = 5;
@@ -119,18 +113,19 @@ export class MainPageComponent implements OnInit, AfterViewInit, OnDestroy {
         this.renderer.setSize(this.windowWidth, this.windowHeight);
         this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
 
-        // Создаем плоскость с видео текстурой
-        const geometry = new THREE.PlaneGeometry(2, 2, 1, 1); // Плоскость теперь будет с соотношением сторон 1:1
+        const geometry = new THREE.PlaneGeometry(2, 2, 1, 1);
         const material = new THREE.MeshBasicMaterial({map: this.videoTexture});
         const plane = new THREE.Mesh(geometry, material);
         this.scene.add(plane);
-
-        // Изначально масштабируем плоскость
         this.adjustPlaneScale(plane);
+        if(this.canvasSize === 500){
+            this.camera.lookAt(1,0,0);
+            return
+        }
+        this.camera.lookAt(0,0,0);
     }
 
     private adjustPlaneScale(plane: THREE.Mesh): void {
-        // Корректировка масштаба плоскости в зависимости от размеров окна
         const scaleX = this.windowWidth / 200;
         const scaleY = this.windowHeight / 200;
         plane.scale.set(scaleX, scaleY, 1);
