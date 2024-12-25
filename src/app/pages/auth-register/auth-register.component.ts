39db3c823a69e70d897424273e28a187bb3e45b9
o.kyrianov <o.kyrianov@fora.ua>
2024-12-25 22:41:47 +0200

    [feat](main page): change logic + connect local server

--- src/app/pages/auth-register/auth-register.component.ts
+++ src/app/pages/auth-register/auth-register.component.ts
@@ -9,6 +9,7 @@ import {MatDivider} from "@angular/material/divider";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
 import {AuthService} from "../../shared/services/auth.service";
 import {MatIcon} from "@angular/material/icon";
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-auth-register',
@@ -18,13 +19,70 @@ import {MatIcon} from "@angular/material/icon";
   imports: [CommonModule, FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatCheckbox, MatDivider, MatSlideToggle, MatIcon],
 })
 export class AuthRegisterComponent implements OnInit{
+  private _snackBar = inject(MatSnackBar);
+  private language: 'en' | 'uk' = 'uk';
   authService: AuthService = inject(AuthService);
   isAuthenticated: boolean = false;
+  sid: string = '';
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
   ngOnInit(): void {
+    this.sid = JSON.parse(localStorage.getItem('appSId')!);
+      this.authService.checkSession(this.sid).subscribe({
+        next: (res: AuthResponse) => {
+          console.log(res);
+          this.openSnackBar(AuthMessageTypeConfig[res.messageType][this.language]);
+          if(res.messageType === 2 && res.sid) this.authService.allowLogIn(true);
+        },
+        error: (error) => {
+          console.log(error);
+          this.openSnackBar('Server error ' + error.message);
+        }
+      });
   }
 
   login() {
-    this.authService.logIn(true);
+    this.authService.logIn({username: 'admin', password: 'password'}).subscribe((res: any) => {
+      console.log(res)
+      // this.authService.allowLogIn(true);
+      if(res.messageType === 3) {
+        this.authService.logedIn(res);
+        this.openSnackBar('З поверненням');
+      }
+    })
   }
+
+  openSnackBar(text: string) {
+    console.log('snack bar opened')
+    this._snackBar.open(text, 'Ok', {
+      duration: 3000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
+}
+
+
+export const AuthMessageTypeConfig = [
+  {
+    en: 'This is your first time on this device, reauthorization/registration',
+    uk: 'Ви перший раз на цьому пристрої, потрібна авторизація/реєстрація'
+  },{
+    en: 'The authorization token has expired, log in again',
+    uk: 'Токен авторизації прострокован, зайдіть заново'
+  },{
+    en: 'You are successfully logged in',
+    uk: 'Ви успішно авторизовані'
+  },{
+    en: '3The authorization token has expired, log in again',
+    uk: '3Токен авторизації прострокован, зайдіть заново'
+  },
+]
+
+export interface AuthResponse {
+  message: string;
+  messageType: number;
+  sid: string
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,17 +1,35 @@
-import { Injectable, signal } from "@angular/core";
-import {ThemeTypes} from "../models/setting-model";
+import {inject, Injectable, signal} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environment";
+import {Observable} from "rxjs";
+import {AuthResponse} from "../../pages/auth-register/auth-register.component";
 
 @Injectable({
     providedIn: "root",
 })
 export class AuthService {
     isAuth = signal<boolean>(false);
+    private http: HttpClient = inject(HttpClient);
 
-    logIn(state: boolean){
+    allowLogIn(state: boolean){
         this.isAuth.update((value) => value = state);
     }
 
     logOut(){
         this.isAuth.update((value) => value = false);
+    }
+
+    logIn(data: any){
+        return this.http.post(environment.developmentUrl + 'Auth/login', {username: 'admin', password: 'password'});
+    }
+
+    checkSession(sid: string): Observable<AuthResponse> {
+        console.log('checkSession вызван');
+        return this.http.post<AuthResponse>(environment.developmentUrl + 'Auth/checkSid', {sid: sid && sid.length ? sid.toString() : ''});
+    }
+
+    logedIn(successResponce: any){
+        this.allowLogIn(true);
+        localStorage.setItem('appSId',JSON.stringify(successResponce.sid))
     }
 }
